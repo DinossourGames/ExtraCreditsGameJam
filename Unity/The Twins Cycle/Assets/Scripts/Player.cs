f39c8e257bc683627a569ed01fec29f79cef32cf
aleffeh <aleffeh@dinossour.com.br>
2019-03-02 20:16:20 -0300

    Phisics bugadasso

---
+++ Unity/The Twins Cycle/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float movementSpeed;
+	public float jumpHeight;
+
+	bool onAir = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float x = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+		float y = Input.GetAxis("Vertical") * jumpHeight * Time.deltaTime;
+		transform.position = new Vector2(transform.position.x + x,transform.position.y);
+
+	}
+}
