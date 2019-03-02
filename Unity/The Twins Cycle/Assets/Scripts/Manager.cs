33cd7b346519dc9f953f3b5d8324f68422e1ecea
aleffeh <aleffeh@dinossour.com.br>
2019-03-02 13:41:19 -0300

    Pause/DeathScreen created
    prefab for pause/death screen created, still needs to implement the script who ll change the atributes

--- Unity/The Twins Cycle/Assets/Scripts/Manager.cs
+++ Unity/The Twins Cycle/Assets/Scripts/Manager.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public void SaveGame()
+	{
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void GameOver(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 	}
 }
