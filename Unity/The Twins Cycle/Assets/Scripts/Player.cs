36aaffda93e8b3dbb649213df590db3d231e9544
aleffeh <aleffeh@dinossour.com.br>
2019-03-03 06:43:50 -0300

    Switch betwen players logic done
    A manager is nescessary to do the core mechanics

--- Unity/The Twins Cycle/Assets/Scripts/Player.cs
+++ Unity/The Twins Cycle/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,7 +16,17 @@ public class Player : MonoBehaviour {
 	public LayerMask whatIsGround;
 
 	private bool facingRight = true;
+	public bool isMain = true;
+	bool switchTriggered = false;
 
+
+	void Awake(){
+		try{
+			PlayerPrefs.DeleteKey("SwitchTrigger");
+		}catch{
+			Debug.Log("Already Deleted");
+		}
+	}
 
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
@@ -24,25 +35,53 @@ public class Player : MonoBehaviour {
 	
 	void Update(){
 
+		switchTriggered = PlayerPrefs.GetInt("SwitchTrigger",0) == 0 ? false : true;
 
-		if((Input.GetKeyDown(KeyCode.JoystickButton1) || Input.GetKeyDown(KeyCode.UpArrow)) && isGrounded){
+		if(isMain){ //EVERYTHING WHAT HAPPENS WHEN PLAYER IS SELECTED NEEDS TO BE PLACED INSIDE THIS IF STATEMENT
 
-			rb.velocity = Vector2.up * jumpForce;
+			if(Input.GetKeyDown(KeyCode.JoystickButton2) || Input.GetKeyDown(KeyCode.Space))
+				SwitchMain();
+
+			if((Input.GetKeyDown(KeyCode.JoystickButton1) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && isGrounded){
+				rb.velocity = Vector2.up * jumpForce;
+			}
+		}else{
+			if(switchTriggered){
+				isMain = true;
+				switchTriggered = false;
+				PlayerPrefs.SetInt("SwitchTrigger",0);
+			}
 		}
 
 	}
 
-	void FixedUpdate(){
+    private void SwitchMain()
+    {
+		isMain = false;
+        PlayerPrefs.SetInt("SwitchTrigger",1);
+    }
 
-		isGrounded = Physics2D.OverlapCircle(groundCheck.position,checkRadius,whatIsGround);
+    void FixedUpdate(){
 
-		moveInput = Input.GetAxis("Horizontal");
-		rb.velocity = new Vector2(moveInput * movementSpeed, rb.velocity.y);
+		if(isMain){ //EVERYTHING WHAT HAPPENS WHEN PLAYER IS SELECTED NEEDS TO BE PLACED INSIDE THIS IF STATEMENT
+			isGrounded = Physics2D.OverlapCircle(groundCheck.position,checkRadius,whatIsGround);
 
-		if(!facingRight && moveInput > 0)
-			Flip();
-		else if(facingRight && moveInput < 0)
-			Flip();
+			moveInput = Input.GetAxis("Horizontal");
+			rb.velocity = new Vector2(moveInput * movementSpeed, rb.velocity.y);
+
+			if(!facingRight && moveInput > 0)
+				Flip();
+			else if(facingRight && moveInput < 0)
+				Flip();
+			
+		}else{
+			if(switchTriggered){
+				isMain = true;
+				switchTriggered = false;
+				PlayerPrefs.SetInt("SwitchTrigger",0);
+			}
+		}
+
 	}
 
 	void Flip(){
