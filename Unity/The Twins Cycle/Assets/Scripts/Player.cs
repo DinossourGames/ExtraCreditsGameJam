7842d4d0af6c7979fa4390788652fab48d92ffcd
aleffeh <aleffeh@dinossour.com.br>
2019-03-03 07:15:17 -0300

    Simple Mechanics Working
    changes betwen player are sooo cute ^^

--- Unity/The Twins Cycle/Assets/Scripts/Player.cs
+++ Unity/The Twins Cycle/Assets/Scripts/Player.cs
@@ -15,18 +15,9 @@ public class Player : MonoBehaviour {
 	public float checkRadius;
 	public LayerMask whatIsGround;
 
-	private bool facingRight = true;
-	public bool isMain = true;
-	bool switchTriggered = false;
+	public bool facingRight = true;
+	public bool isMain ;
 
-
-	void Awake(){
-		try{
-			PlayerPrefs.DeleteKey("SwitchTrigger");
-		}catch{
-			Debug.Log("Already Deleted");
-		}
-	}
 
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
@@ -35,31 +26,21 @@ public class Player : MonoBehaviour {
 	
 	void Update(){
 
-		switchTriggered = PlayerPrefs.GetInt("SwitchTrigger",0) == 0 ? false : true;
+		// switchTriggered = PlayerPrefs.GetInt("SwitchTrigger",0) == 0 ? false : true;
 
 		if(isMain){ //EVERYTHING WHAT HAPPENS WHEN PLAYER IS SELECTED NEEDS TO BE PLACED INSIDE THIS IF STATEMENT
-
-			if(Input.GetKeyDown(KeyCode.JoystickButton2) || Input.GetKeyDown(KeyCode.Space))
-				SwitchMain();
 
 			if((Input.GetKeyDown(KeyCode.JoystickButton1) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && isGrounded){
 				rb.velocity = Vector2.up * jumpForce;
 			}
-		}else{
-			if(switchTriggered){
-				isMain = true;
-				switchTriggered = false;
-				PlayerPrefs.SetInt("SwitchTrigger",0);
-			}
 		}
-
+		// else{
+		// 	if(switchTriggered){
+		// 		isMain = true;
+		// 		switchTriggered = false;
+		// 		PlayerPrefs.SetInt("SwitchTrigger",0);
+		// 	}
 	}
-
-    private void SwitchMain()
-    {
-		isMain = false;
-        PlayerPrefs.SetInt("SwitchTrigger",1);
-    }
 
     void FixedUpdate(){
 
@@ -73,13 +54,7 @@ public class Player : MonoBehaviour {
 				Flip();
 			else if(facingRight && moveInput < 0)
 				Flip();
-			
-		}else{
-			if(switchTriggered){
-				isMain = true;
-				switchTriggered = false;
-				PlayerPrefs.SetInt("SwitchTrigger",0);
-			}
+
 		}
 
 	}
---
+++ Unity/The Twins Cycle/Assets/Scripts/TimeCoreManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeCoreManager : MonoBehaviour
+{
+
+    public GameObject[] playerPrefabs; // 0 --> moon 1 --> sun
+
+    bool whoIs; //true --> moon  false --> sun
+   
+
+    void Start()
+    {
+        whoIs = playerPrefabs[0].GetComponent<Player>().isMain ? true : false;
+    }
+
+
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.JoystickButton2) || Input.GetKeyDown(KeyCode.Space))
+            SwitchChar();
+    }
+
+    void SwitchChar(){
+        if(whoIs){
+            whoIs = false;
+            playerPrefabs[0].GetComponent<Player>().isMain = false;
+            playerPrefabs[1].GetComponent<Player>().isMain = true;
+        }else{
+            whoIs = true;
+            playerPrefabs[1].GetComponent<Player>().isMain = false;
+            playerPrefabs[0].GetComponent<Player>().isMain = true;
+        }
+    }
+}
+
+
